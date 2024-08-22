13f771a2e8295319fbd4c54fb6dec4973efce96b
Fernando Rodriguez <fernando.ale.fars@gmail.com>
2024-08-22 09:29:48 -0400

    Tests hasta Fizz para 3

---
+++ src/fizzbuzz.js
@@ -0,0 +1,7 @@
+function generarFizzBuzz(numero){
+    if(numero==3)
+    return "Fizz";
+    else return numero;
+
+}
+export default generarFizzBuzz;
---
+++ src/fizzbuzz.spec.js
@@ -0,0 +1,14 @@
+import generarFizzBuzz from './fizzbuzz'
+describe('FizzBuzz',()=>{
+    it ("Generar el mismo numero si no sigue ninguna regla",()=>{
+        expect(generarFizzBuzz(1)).toEqual(1);
+    });
+    it("generar el mismo numero para cualquiier numero si no sigue ninguna regla",()=>{
+        expect(generarFizzBuzz(2)).toEqual(2);
+    });
+    it("Generar Fizz para el 3",()=>{
+        expect(generarFizzBuzz(3)).toEqual('Fizz');
+    });
+
+});
+
